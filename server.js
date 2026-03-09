d9af294aa65baedfb5f04d34c8085d578543fb33
THEHACKILLER17 <shaikhzaid0903@gmail.com>
2026-03-09 15:44:11 +0530

    update feature

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000;
 // Core middleware
 app.use(express.json());
 app.use(cors({
-  origin: "https://food-orders-web.netlify.app",
+  origin: ["http://localhost:5173", "https://food-orders-web.netlify.app"],
   credentials: true,
 }));
 
